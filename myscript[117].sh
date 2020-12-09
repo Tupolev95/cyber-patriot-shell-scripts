1233a1230cf20eb948271a8488e45b9d4b219299
Tupolev95 <75495367+Tupolev95@users.noreply.github.com>
2020-12-09 08:43:57 -0800

    Update myscript[117].sh

--- myscript[117].sh
+++ myscript[117].sh
@@ -11,6 +11,7 @@ sudo auditctl -w /etc/shadow -p wa
 sudo auditctl -w /etc/passwd -p wa
 sudo auditctl -w /etc/group -p wa
 sudo auditctl -w /etc/sudoers -p wa
+sudo auditctl -e 1
 sudo gedit /etc/pam.d/common-password #at the end of line 25 add remember=5 minlen=10 difok=3 ucredit=-1 lcredit=-1 dcredit=-1 ocredit=-1
 sudo gedit /etc/pam.d/common-auth #make a new line at the end and write auth required pam_tally2.so deny=5 onerr=fail unlock_time=1800
 sudo gedit /etc/login.defs #starting at line 160, change values to 90, 10 and 7
