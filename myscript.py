b0bda409c078f02dac32444c8fd12b9fb6a08f1e
Tupolev95 <75495367+Tupolev95@users.noreply.github.com>
2021-08-21 21:33:03 -0600

    Add files via upload

--- myscript.py
+++ myscript.py
@@ -1,7 +1,10 @@
-import os
-cmd = 'ls -l'
-os.system(cmd)
 
+##this section of the program is meant to remove unathorized users
+## you must first input how many authorized users according to the readme you should have, then thier names, then it will promt you to delete users not on said list 
+##NOTE!!! DO NOT TYPE Y FOR EVERY USER THAT IT PROMPTS YOU TO DELETE! some of them may be system accounts, if you wish, investigate further
+
+
+import os
 
 
 ##amount of users
@@ -24,8 +27,7 @@ users2 = []
 
 p = []
 import pwd, grp
-for p in pwd.getpwall():
-	print(p[0], grp.getgrgid(p[3])[0])
+
 
 #this prints all JUST users in the system
 for p in pwd.getpwall():
@@ -37,29 +39,33 @@ print("users2=", users2)
 
 masterlist = ["root", "deamon", "bin", "sys", "sync", "games", "man", "lp", "mail", "news", "uucp", "proxy", "www-data", "backup", "list", "irc", "gnats", "nobody", "systemd-network", "systemd-resolve", "systemd-timesync", "messagebus", "syslog", "_apt", "tss", "uuidd", "tcpdump", "avahi-autoipd", "usbmux", "rtkit", "dnsmasq", "cups-pk-helper", "speech-dispatcher", "speed-dispatcher", "avahi", "kernoops", "saned", "nm-openvpn", "hplip", "whoopsie", "colord", "geoclue", "pulse", "gnome-initial-setup", "gdm", "systemd-coredump", "root"]
 
+##adds all the users typed in at the beginning from the read me to the masterlist
 masterlist.extend(users)
 
 print(masterlist)
 
 
-
+## this creates the list users3 which is the diffrence between users2 and the masterlist
 users3 = set(users2) ^ set(masterlist)
 
 print("users3 =", users3)
 
-##import getent
-##this was an attempt to use getent but it didnt work :(
-##print(dict(getent.passwd('root')))
 
 
 import subprocess
+import sys
+import getpass
+
+for x in users3:
+	yn = input("would you like to delete user" + x + "? ")
+	if yn == "y":
+		output = subprocess.run(['userdel', x ])
+			
+			
 
 
 
-##process = subprocess.run(['getent','passwd', '{1000..60000}'], stdout=subprocess.PIPE)
-	
-##print(process.stdout.decode())
 
-print(os.popen("getent passwd {1000..60000}").read())
+
 
 
